807e35d114cdce6bef763871b04fd544061903a8
Faizur Rahman <faizur11786@gmail.com>
2025-02-10 23:01:47 +0530

    some bug fixed

--- src/heros/config.ts
+++ src/heros/config.ts
@@ -17,7 +17,7 @@ export const hero: Field = {
     {
       name: "type",
       type: "select",
-      defaultValue: "lowImpact",
+      defaultValue: "none",
       label: "Type",
       options: [
         {
